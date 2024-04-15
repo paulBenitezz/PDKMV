4afe08984dd9f6a865e9c49c499e22731b71d35f
paulBenitezz <pbenitez690@gmail.com>
2024-04-15 14:47:11 -0500

    UI and Audio

--- PDKMV/Assets/Scripts/AISpawner.cs
+++ PDKMV/Assets/Scripts/AISpawner.cs
@@ -1,17 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class AISpawner : MonoBehaviour
 {
     
     public GameObject[] SpawnLocations;
     public GameObject[] EnemyType;
-
+    [SerializeField] float SpawnRate = 3f;
 
     void Start() {
 
-        InvokeRepeating("Spawn", 1.5f, 3);
+        InvokeRepeating("Spawn", 1.5f, SpawnRate);
     }
 
     void Spawn() {
@@ -19,9 +21,9 @@ public class AISpawner : MonoBehaviour
         int randEne = Random.Range(0,EnemyType.Length);
 
         Vector3 pos = SpawnLocations[randLoc].transform.position;
-        transform.position = pos;
+        //transform.position = pos;
         Debug.Log("Spawn Location " + randLoc + " position: " + pos);
-        GameObject newEnemy = Instantiate(EnemyType[randEne], transform.position, transform.rotation);
+        GameObject newEnemy = Instantiate(EnemyType[randEne], pos, quaternion.identity);
         
 
     }
